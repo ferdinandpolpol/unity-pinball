714d70dee6d4b23831e0eee863bf13af435f093f
Ferdinand Polpol <ferdinand.polpol@gmail.com>
2019-03-03 13:45:20 +0800

    added files

---
+++ Pinball/Assets/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameObject currentBallToJump;
+    public GameObject[] balls;
+    public int currentCount = 0;
+    public InputField input;
+    private int jumpForce = 37;
+    // Start is called before the first frame update
+    void Start()
+    {
+      // jumpForce = GameObject.Find
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      int.TryParse(input.text, out jumpForce);
+      if(Input.GetKeyDown("f")){
+        if(jumpForce < 37){
+            Debug.Log("Please input a number")
+        } else {
+        Debug.Log("pressed F");
+            try{
+              GameObject currBall = balls[currentCount];
+              Debug.Log(currBall);
+              currBall.GetComponent<Rigidbody>().AddForce(0,( 30 * jumpForce) ,0);
+              currentCount++;
+            }
+            catch(Exception e){
+              Debug.Log(e);
+            }
+        }
+      }
+    }
+}
---
+++ Pinball/Assets/Scorer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorer : MonoBehaviour
+{
+    public Text holeText;
+
+    void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "PlayBalls") {
+            holeText.text += "X";
+        }
+    }
+}
